c0d000a7330e2d3477f86c63d55b2952d07b0530
Danilo Righetto <danilonewtrue@gmail.com>
2021-06-22 17:09:02 -0300

    python strings

---
+++ strings/strings.py
@@ -0,0 +1,13 @@
+"""
+str = string
+"""
+
+# O python e uma linguagem de TIPAGEM DINAMICA
+print('Testando uma string')
+print("Testando outra string")
+
+print("Aspas 'simples'")
+print('Aspas "duplas"')
+
+print("Esse é o meu \"texto\"") # Utilizando a "\" para escapar o caracter
+print(r'Esse é o meu \n texto') # Utilizando sem scape com o 'r' de raw.
