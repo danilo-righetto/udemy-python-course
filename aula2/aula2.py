87e266436956476c2b12d5fbece81c70d268ee6b
Danilo Righetto <danilonewtrue@gmail.com>
2021-06-22 16:59:54 -0300

    print command

---
+++ aula2/aula2.py
@@ -0,0 +1,7 @@
+# Utilizando o comando print
+print(123456)
+print('Danilo', 'Righetto')
+print('Ana', 'Clara', sep='-') # Usando um separador
+print('Cassy', 'Jones', end='## ') # Usando um texto pro final do print
+
+print('856', '658', '058', sep='.', end='-58')
