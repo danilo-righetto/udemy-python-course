305fd132cb101296a884ae6d1ec58da009955ec0
Danilo Righetto <danilonewtrue@gmail.com>
2021-06-22 16:49:34 -0300

    code comments

--- aula01.py
+++
@@ -1,1 +0,0 @@
-print('Hello World - Danilo Righetto')
---
+++ aula1/aula1.py
@@ -0,0 +1,9 @@
+# Isso e um comentario
+print('linha 1')
+print('linha 2')
+print('linha 3')
+print('linha 4')
+""" isso e outro comentario
+print('linha 5')
+print('linha 6')
+"""
